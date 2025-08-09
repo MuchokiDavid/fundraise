bda4b961499fa85c53dcf4457cf1eb75c51af4ef
david <dmmuchoki7@gmail.com>
2025-08-10 02:29:57 +0300

    Configures Django Admin for profiles
    Sets up comprehensive admin interfaces for the User, Campaign_Owner, and Donor models. This enhances administrative control and data visibility by defining list displays, filters, search capabilities, ordering, read-only fields, and fieldsets for each model.
    
    Removes the CustomUserManager file, indicating a simplification or refactoring of custom user management logic.

--- fundraiser/profiles/admin.py
+++ fundraiser/profiles/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'firstname', 'lastname', 'email', 'user_type', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['phone_number', 'email', 'national_id']
+    ordering = ['id']
+    readonly_fields = ['id', 'date_joined', 'last_login']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('firstname', 'lastname', 'national_id', 'phone_number', 'user_type')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+@admin.register(Campaign_Owner)
+class Campaign_OwnerAdmin(admin.ModelAdmin):
+    list_display = ['user', 'organization_name', 'organization_description', 'organization_website', 'organization_logo', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['user__firstname', 'user__lastname', 'organization_name']
+    ordering = ['user__id']
+    readonly_fields = ['created_at', 'updated_at']
+
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['user__firstname', 'user__lastname']
+    ordering = ['user__id']
+    readonly_fields = ['created_at', 'updated_at']
